4d0962a74ef490f65e6b7b517ef2b5b213f6bc32
전승원 <seungwon7308@gmail.com>
2023-11-07 20:46:21 +0900

    project 기본 세팅

---
+++ src/main/kotlin/com/example/joalarm/JoAlarmApplication.kt
@@ -0,0 +1,11 @@
+package com.example.joalarm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoAlarmApplication
+
+fun main(args: Array<String>) {
+    runApplication<JoAlarmApplication>(*args)
+}
